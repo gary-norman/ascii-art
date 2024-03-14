d847faf7c99f260a67a083b6509bc6c5bbecba2f
Gary Norman <gary.norman.th@gmail.com>
2024-03-14 11:28:13 +0000

    added functionality (excludes \n)

--- ascii-art.go
+++ ascii-art.go
@@ -41,10 +41,21 @@ abcdefghijklmnopqrstuvwxyz
 https://github.com/01-edu/public/blob/master/subjects/ascii-art/audit/README.md
 
 */
+// removeEmptyStrings - Use this to remove empty string values inside an array.
+func removeEmptyStrings(s []string) []string {
+	var r []string
+	for _, str := range s {
+		if str != "" {
+			r = append(r, str)
+		}
+	}
+	return r
+}
 
 func main() {
 	// save argument as a variable
 	text := os.Args[1]
+	textarr := strings.Split(text, "\\n")
 	// import file of target ascii
 	file, err := os.ReadFile("standard.txt")
 	if err != nil {
@@ -55,10 +66,14 @@ func main() {
 	// assign this to a variable, split by endlines
 	source := strings.Split(string(file), "\n")
 
-	for i := 1; i < 10; i++ {
-		for _, char := range text {
-			fmt.Print(source[(int(char)-(baseline))*9+(i)])
+	textarr = removeEmptyStrings(textarr)
+
+	for i := 0; i < len(textarr); i++ {
+		for j := 1; j < 10; j++ {
+			for _, char := range textarr[i] {
+				fmt.Print(source[(int(char)-(baseline))*9+(j)])
+			}
+			fmt.Println("")
 		}
-		fmt.Println("")
 	}
 }
