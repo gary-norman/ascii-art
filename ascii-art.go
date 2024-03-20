e21b3fe07ad2d4fb6035265d463b0a9df924cb90
Gary Norman <gary.norman.th@gmail.com>
2024-03-20 09:12:46 +0000

    cleaned up code

--- ascii-art.go
+++ ascii-art.go
@@ -25,7 +25,7 @@ import (
 
 */
 
-// import standard.txt as the default ascii style, with ability to change it
+// PrepareBan import standard.txt as the default ascii style, with ability to change it
 // using 2nd argument *
 // ^^ thinkertoy does not work
 func PrepareBan(bannerStyle string) []string {
@@ -43,7 +43,10 @@ func PrepareBan(bannerStyle string) []string {
 	for scanned.Scan() {
 		source = append(source, scanned.Text())
 	}
-	file.Close()
+	errClose := file.Close()
+	if errClose != nil {
+		return nil
+	}
 	return source
 }
 
@@ -69,7 +72,7 @@ func makeArt(origString string, y map[int][]string) string {
 		for j := 0; j < len(y[32]); j++ {
 			var line string
 			for _, letter := range word {
-				line = line + string((y[int(letter)][j]))
+				line = line + y[int(letter)][j]
 			}
 			art += line + "\n"
 			line = ""
@@ -91,6 +94,7 @@ func Contains(slice []rune, item rune) bool {
 // transform the input text origString to the output art, line by line, colorizing specified text
 func makeArtColorized(origString string, y map[int][]string, letters []rune, color string, colorAll bool) string {
 	var specifiedColor string
+	reset := "\033[0m"
 	switch color {
 	case "red":
 		specifiedColor = "\033[31m"
@@ -100,8 +104,9 @@ func makeArtColorized(origString string, y map[int][]string, letters []rune, col
 		specifiedColor = "\033[33m"
 	case "blue":
 		specifiedColor = "\033[34m"
+	default:
+		fmt.Print("\nAvailable colors are " + "\033[31m" + "red" + reset + ", " + "\033[32m" + "green" + reset + ", " + "\033[33m" + "yellow" + reset + ", and " + "\033[34m" + "blue" + reset + ".\n")
 	}
-	reset := "\033[0m"
 	var art string
 	replaceNewline := strings.ReplaceAll(origString, "\r\n", "\\n") // correct newline formatting
 	wordSlice := strings.Split(replaceNewline, "\\n")
@@ -110,14 +115,14 @@ func makeArtColorized(origString string, y map[int][]string, letters []rune, col
 			var line string
 			if colorAll {
 				for _, letter := range word {
-					line = specifiedColor + line + string((y[int(letter)][j]))
+					line = specifiedColor + line + y[int(letter)][j]
 				}
 			} else {
 				for _, letter := range word {
 					if Contains(letters, letter) {
-						line = line + specifiedColor + string((y[int(letter)][j])) + reset
+						line = line + specifiedColor + y[int(letter)][j] + reset
 					} else {
-						line = line + string((y[int(letter)][j]))
+						line = line + y[int(letter)][j]
 					}
 				}
 			}
@@ -129,19 +134,19 @@ func makeArtColorized(origString string, y map[int][]string, letters []rune, col
 }
 
 func main() {
-
 	// ? flag definitions
 	reverse := flag.String("reverse", "default", "Tell the program to run the reverse function")
 	color := flag.String("color", "default", "Tell the program to run the color function")
 	output := flag.String("output", "default", "Save the output to the specified filename")
 	align := flag.String("align", "default", "Tell the program to run the align function")
-	flag.Parse()
-	additionalArgs := flag.Args()
+	flag.Parse()                  // parse the flags so that they can be used
+	additionalArgs := flag.Args() // tell the program to treat every argument following the flag from arg[o]
 	input := additionalArgs[0]
 	var bannerStyle string
 	if len(additionalArgs) == 2 {
 		bannerStyle = additionalArgs[1]
 	}
+	// TODO complete reverse project
 	if *reverse != "default" {
 		fmt.Printf("Reverse flag is set to  %v\n", *reverse)
 		return
@@ -151,6 +156,7 @@ func main() {
 		var colorAll bool
 		var colSLice []rune
 		colorAll = true
+		// TODO add error/instruction message
 		if len(additionalArgs) == 2 {
 			colorAll = false
 			colored = additionalArgs[0]
@@ -161,6 +167,7 @@ func main() {
 		return
 	}
 	if *output != "default" {
+		// TODO add error/instruction message
 		err := os.WriteFile(*output, []byte(makeArt(input, getChars(PrepareBan(bannerStyle)))), 0644)
 		if err != nil {
 			log.Fatal(err)
@@ -169,10 +176,12 @@ func main() {
 		fmt.Printf("Output has been saved to %v\n", *output)
 		return
 	}
+	// TODO complete align project
 	if *align != "default" {
 		fmt.Printf("align flag is set to  %v\n", *align)
 		return
 	} else {
+		// TODO add error/instruction message
 		fmt.Println(makeArt(input, getChars(PrepareBan(bannerStyle))))
 	}
 }
